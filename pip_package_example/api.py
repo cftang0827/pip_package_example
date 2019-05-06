f1c02bf6eabfecf687593dab1827accd48afa56c
cftang_home <cftang0827@gmail.com>
2019-05-07 03:00:38 +0800

    fix model path problem

--- pip_package_example/api.py
+++ pip_package_example/api.py
@@ -1,9 +1,12 @@
 import json
 import numpy as np
+import os
 
-with open("pip_package_example/data/model.json", 'r') as f:
+with open(os.path.join(os.path.dirname(__file__), "data/model.json"),
+          'r') as f:
     model_data = json.load(f)
 
 print(model_data["name"])
 print(model_data["token"])
 print(np.array([0]))
+print("Read model success")
